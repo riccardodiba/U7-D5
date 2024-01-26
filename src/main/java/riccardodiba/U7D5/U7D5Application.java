e3e7079e44a417141fa5ae499c8bc7eac400ae50
Riccardo Di Bari <riccardodibari@hotmail.it>
2024-01-26 10:51:24 +0100

    creazione progetto

---
+++ src/main/java/riccardodiba/U7D5/U7D5Application.java
@@ -0,0 +1,13 @@
+package riccardodiba.U7D5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U7D5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U7D5Application.class, args);
+	}
+
+}
---
+++ src/test/java/riccardodiba/U7D5/U7D5ApplicationTests.java
@@ -0,0 +1,13 @@
+package riccardodiba.U7D5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U7D5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
